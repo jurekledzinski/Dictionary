cb323d06aca4d55c81fb66ee569cf6993c8466e4
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-08-12 23:33:09 +0200

    Create languages data to loop through it dynamically and display tails

---
+++ src/components/tailsLanguages/TailsData.js
@@ -0,0 +1,86 @@
+export const langsArr = [
+  {
+    id: 0,
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/dictionary-cc280.appspot.com/o/Saudi%20Arabia%20icon.png?alt=media&token=9fdec684-f060-4945-8180-5e34b0d202c1",
+    alt: "Arabic",
+    nameLang: "Arabic",
+  },
+  {
+    id: 1,
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/dictionary-cc280.appspot.com/o/Brazil%20icon.png?alt=media&token=57e150f6-b1e5-4622-84f4-386ac3178047",
+    alt: "Brasil Portuguese",
+    nameLang: "Brasil Portuguese",
+  },
+  {
+    id: 2,
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/dictionary-cc280.appspot.com/o/Great%20Britain%20icon.png?alt=media&token=376bac8c-c516-4409-aff3-25654e9fd55b",
+    alt: "English",
+    nameLang: "English",
+  },
+  {
+    id: 3,
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/dictionary-cc280.appspot.com/o/France%20icon.png?alt=media&token=b3d17825-6732-438d-8c13-97da5404776c",
+    alt: "French",
+    nameLang: "French",
+  },
+  {
+    id: 4,
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/dictionary-cc280.appspot.com/o/Germany%20icon.png?alt=media&token=49ea07c3-8d29-494c-9d1a-1a68167ef597",
+    alt: "German",
+    nameLang: "German",
+  },
+  {
+    id: 5,
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/dictionary-cc280.appspot.com/o/India%20icon.png?alt=media&token=a4e4f081-19fc-4ba4-b4f4-265434c6915a",
+    alt: "Hindi",
+    nameLang: "Hindi",
+  },
+  {
+    id: 6,
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/dictionary-cc280.appspot.com/o/Italy%20icon.png?alt=media&token=ca748c3e-44d6-4971-b96f-8dd8522acacb",
+    alt: "Italian",
+    nameLang: "Italian",
+  },
+  {
+    id: 7,
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/dictionary-cc280.appspot.com/o/Japan%20icon.png?alt=media&token=209b77b1-3e37-41a1-80f3-6874381daaf8",
+    alt: "Japanese",
+    nameLang: "Japanese",
+  },
+  {
+    id: 8,
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/dictionary-cc280.appspot.com/o/South%20Korea%20icon.png?alt=media&token=3b0f38f0-3878-4983-a9aa-18857eb7fe9a",
+    alt: "Korean",
+    nameLang: "Korean",
+  },
+  {
+    id: 9,
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/dictionary-cc280.appspot.com/o/Russian%20Federation%20icon.png?alt=media&token=cb25a9f8-af96-40e2-a849-ff8a3c2f3ea3",
+    alt: "Russian",
+    nameLang: "Russian",
+  },
+  {
+    id: 10,
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/dictionary-cc280.appspot.com/o/Spain%20icon.png?alt=media&token=d7e34224-2754-4d88-acea-ddc6a8a18193",
+    alt: "Spanish",
+    nameLang: "Spanish",
+  },
+  {
+    id: 11,
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/dictionary-cc280.appspot.com/o/Turkey%20icon.png?alt=media&token=9da251cc-a88e-4943-8159-949c6f79607d",
+    alt: "Turkish",
+    nameLang: "Turkish",
+  },
+];
