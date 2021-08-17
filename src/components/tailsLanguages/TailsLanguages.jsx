9df88b09448bc294f52f7e7975b5bf906db25866
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-08-17 09:30:16 +0200

    Create tails languages component and show tails or details of language depends of search language

--- src/components/tailsLanguages/TailsLanguages.jsx
+++ src/components/tailsLanguages/TailsLanguages.jsx
@@ -1,43 +1,17 @@
-import React, { useState } from "react";
+import React, { Fragment, useContext } from "react";
 
-import "./TailsLanguages.scss";
+import { StoreContext } from "../../store/StoreProvider";
 
-import { langsArr } from "./TailsData";
+import Tails from "./Tails";
+import DetailsLanguage from "./DetailsLanguage";
 
 const TailsLanguages = () => {
-  const [idLang, setIdLang] = useState(false);
-
-  const handleSelectLanguage = (id) => {
-    setIdLang(id);
-  };
+  const { dataLanguage } = useContext(StoreContext);
 
   return (
-    <section className="languages">
-      {langsArr.map((item, index) => (
-        <article
-          className={
-            index === idLang
-              ? "languages__tail languages__tail--active"
-              : "languages__tail"
-          }
-          key={item.id}
-          onClick={() => handleSelectLanguage(item.id)}
-        >
-          <div className="languages__icon-wrapper">
-            <img
-              className={
-                index === idLang
-                  ? "languages__icon languages__icon--active"
-                  : "languages__icon"
-              }
-              src={item.image}
-              alt={item.alt}
-            />
-          </div>
-          <p className="languages__name">{item.nameLang}</p>
-        </article>
-      ))}
-    </section>
+    <Fragment>
+      {dataLanguage.length < 1 ? <Tails /> : <DetailsLanguage />}
+    </Fragment>
   );
 };
 
