b990443fc6bcaabe03854358bd3b933b0744e036
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-08-17 09:20:09 +0200

    Fetch data for dictionary

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,22 +1,130 @@
-import React, { useState } from "react";
+import React, {
+  Fragment,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 import "./Form.scss";
 
+import { StoreContext } from "../../store/StoreProvider";
+
 const Form = () => {
+  const {
+    codeLanguage,
+    chooseLanguage,
+    dataLanguage,
+    idLang,
+    setCodeLanguage,
+    setChooseLanguage,
+    setDataLanguage,
+    setIdLang,
+  } = useContext(StoreContext);
+  const [errorMsg, setErrMessage] = useState("");
+  const [isEmpty, setIsEmpty] = useState(true);
+  const [textSearch, setTextSearch] = useState("");
+
+  let idTimeout = useRef(null);
+
   const handleSubmitSearch = (e) => {
     e.preventDefault();
-    console.log("click submit");
+
+    if (Boolean(dataLanguage[0]) && dataLanguage[0].word === textSearch) {
+      setErrMessage("You already find word in this language");
+      return;
+    }
+
+    if (idLang === null) {
+      setErrMessage("Please choose language");
+      return;
+    }
+
+    if (textSearch.length <= 1) {
+      setErrMessage("Word need at least two characters");
+      return;
+    }
+
+    let urlApi = `https://api.dictionaryapi.dev/api/v2/entries/${codeLanguage}/${textSearch}`;
+
+    fetch(urlApi)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Can't find given word");
+        }
+
+        return response.json();
+      })
+      .then((data) => {
+        setDataLanguage(data);
+      })
+      .catch((err) => {
+        setErrMessage(err.message);
+      });
   };
 
+  const handleInputSearch = (e) => {
+    if (e.target.value !== "") {
+      setIsEmpty(false);
+    } else {
+      setIsEmpty(true);
+    }
+
+    setTextSearch(e.target.value);
+  };
+
+  const handleClearLanguage = () => {
+    setCodeLanguage(null);
+    setChooseLanguage(null);
+    setIdLang(null);
+    setIsEmpty(true);
+    setTextSearch("");
+    setDataLanguage([]);
+  };
+
+  useEffect(() => {
+    idTimeout = setTimeout(() => {
+      setErrMessage(null);
+    }, 1500);
+
+    return () => clearTimeout(idTimeout);
+  }, [errorMsg]);
+
   return (
-    <header className="header">
-      <form action="" className="form">
-        <input type="text" className="form__input" />
-        <button className="form__btn-submit" onClick={handleSubmitSearch}>
-          <i className="fas fa-search"></i>
-        </button>
-      </form>
-    </header>
+    <Fragment>
+      {errorMsg && <p className="error-message">{errorMsg}</p>}
+      <header className="header">
+        <form action="" className="form">
+          <input
+            type="text"
+            className={
+              isEmpty ? "form__input" : "form__input form__input--not-empty"
+            }
+            value={textSearch}
+            onChange={handleInputSearch}
+          />
+          <button
+            disabled={isEmpty}
+            className="form__btn-submit"
+            type="submit"
+            onClick={handleSubmitSearch}
+          >
+            <i className="fas fa-search"></i>
+          </button>
+        </form>
+        {chooseLanguage && (
+          <div className="header__info-language">
+            <p className="header__info-text">{chooseLanguage}</p>
+            <button
+              className="header__btn-delete-lang"
+              onClick={handleClearLanguage}
+            >
+              <i className="fas fa-times"></i>
+            </button>
+          </div>
+        )}
+      </header>
+    </Fragment>
   );
 };
 
